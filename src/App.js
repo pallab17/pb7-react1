939238df83478f89e9f861a61363316520be55ab
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-21 18:05:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-      WELCOME TO PB7 KINGDOM
-    </div>
-  )
-}
+    // <div>
+    //   WELCOME TO PB7 KINGDOM
+    //   <p className='yoyo'> yo</p>
+    // </div>
+    // using react fragment ka symbol
+    <>
+      <Pallab />
+      <Pallab />
+      <Pallab />
+      <Pallab />
 
-export default App
+      WELCOME TO PB7 KINGDOM {3 + 3}
+      <p className="yoyo"> yo</p>
+      <Pallab/>
+    </>
+  );
+};
+
+const Pallab = () => {
+  // 1 way of doing this
+  // return  (
+  // <h1>pallab </h1>
+  // );
+  // 2nd way of doing this
+  return <h1>pallab</h1>;
+};
+
+export default App;
